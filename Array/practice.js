a08b5764bb8010bd27996d182249f98b3b4d5c32
yuvraj Sinh <yuvrajsinh123428@gmail.com>
2024-06-22 20:39:38 +0530

    class, inheritance,duplicate

---
+++ Array/practice.js
@@ -0,0 +1,2 @@
+const data=[1,4,30,50,93,50,30]
+console.log(new Set(data));
---
+++ sorting.js
@@ -0,0 +1,6 @@
+function sorrt(data){
+    console.log(data.sort((a,b)=>a-b));
+}
+
+const dat=[1,5,39,50,3,29]
+sorrt(dat)
