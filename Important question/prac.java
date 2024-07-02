a8de6278a44ed2c9b11aab783b6c8ab64816a2ef
yuvraj Sinh <yuvrajsinh123428@gmail.com>
2024-07-02 19:19:11 +0530

    practice for java

---
+++ Important question/prac.java
@@ -0,0 +1,12 @@
+class prac{
+    public static void main(String[] args) {
+        int arr[]={43,65,17,50,33};
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i]>30){
+                System.out.println("greater then 50 : "+arr[i]);
+            }
+
+        }
+    }
+    
+}
